eb481533bc63dc9352c143213b8bbedd4c05123d
Immaculata Emmanuel <e.emmanuel@alustudent.com>
2025-08-21 13:12:38 -0700

    Created 1-last_digit

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-# Your code goes here - complete the logic to find and categorize
-# the last digit
 last_digit = number % 10
 if number < 0 and last_digit != 0:
     last_digit = last_digit - 10
@@ -11,4 +9,5 @@ if last_digit > 5:
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
 else:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    print(f"Last digit of {number} is {last_digit} and is less than 6 "
+          f"and not 0")
